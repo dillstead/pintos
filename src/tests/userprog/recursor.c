421148a51f97254bd53d8a3d5965f8ea92c36684
Ben Pfaff <blp@cs.stanford.edu>
2004-09-22 00:29:01 +0000

    Make more useful.

--- src/tests/userprog/recursor.c
+++ src/tests/userprog/recursor.c
@@ -19,13 +19,16 @@ main (int argc, char *argv[])
   printf ("%s %s %s %s\n", argv[0], argv[1], argv[2], argv[3]);
 
   /* Execute child and wait for it to finish if requested. */
-  snprintf (buffer, sizeof buffer,
-            "recursor fork %d %s", atoi (argv[2]) + 1, argv[3]);
-  pid = exec (buffer);
-  if (atoi (argv[3]))
-    retval = join (pid);
-
+  if (atoi (argv[2]) != 0) 
+    {
+      snprintf (buffer, sizeof buffer,
+                "recursor %s %d %s", argv[1], atoi (argv[2]) - 1, argv[3]);
+      pid = exec (buffer);
+      if (atoi (argv[3]))
+        retval = join (pid);
+    }
+  
   /* Done. */
-  printf ("%s: dying, retval=%d\n", argv[1], retval);
+  printf ("%s %s: dying, retval=%d\n", argv[1], argv[2], retval);
   exit (retval);
 }
