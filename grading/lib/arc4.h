0ec49949304b13ff22287d7d9e3dcb05090c61a4
Ben Pfaff <blp@cs.stanford.edu>
2004-12-07 02:34:09 +0000

    Grading library files.

---
+++ grading/lib/arc4.h
@@ -0,0 +1,18 @@
+#ifndef ARC4_H
+#define ARC4_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+/* Alleged RC4 algorithm encryption state. */
+struct arc4
+  {
+    uint8_t s[256];
+    uint8_t i, j;
+  };
+
+void arc4_init (struct arc4 *, const void *, size_t);
+void arc4_crypt (struct arc4 *, void *, size_t);
+
+
+#endif /* arc4.h */
---
+++ grading/lib/cksum.h
@@ -0,0 +1,8 @@
+#ifndef CKSUM_H
+#define CKSUM_H
+
+#include <stddef.h>
+
+unsigned long cksum(const unsigned char *b, size_t n);
+
+#endif /* cksum.h */
