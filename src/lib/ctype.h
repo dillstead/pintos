9e39e3f94c83c6fc61ba7f620677b5210c0c15be
Ben Pfaff <blp@cs.stanford.edu>
2005-04-01 18:43:22 +0000

    127 is a control character.

--- src/lib/ctype.h
+++ src/lib/ctype.h
@@ -14,9 +14,9 @@ static inline int isspace (int c) {
           || c == '\r' || c == '\t' || c == '\v');
 }
 static inline int isblank (int c) { return c == ' ' || c == '\t'; }
-static inline int isgraph (int c) { return c >= 33 && c < 127; }
+static inline int isgraph (int c) { return c > 32 && c < 127; }
 static inline int isprint (int c) { return c >= 32 && c < 127; }
-static inline int iscntrl (int c) { return c >= 0 && c < 32; }
+static inline int iscntrl (int c) { return c >= 0 && c < 32 || c == 127; }
 static inline int isascii (int c) { return c >= 0 && c < 128; }
 static inline int ispunct (int c) {
   return isprint (c) && !isalnum (c) && !isspace (c);
