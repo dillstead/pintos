42428b5613cb885d3d1fa9933f6dc51f5a6c8019
Ben Pfaff <blp@cs.stanford.edu>
2006-01-04 18:56:16 +0000

    Move PF_* from exception.c to exception.h, because some students may wish to pass them around. Suggested by Godmar Back.

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -108,11 +108,6 @@ kill (struct intr_frame *f)
     }
 }
 
-/* Page fault error code bits that describe the cause of the exception.  */
-#define PF_P 0x1    /* 0: not-present page. 1: access rights violation. */
-#define PF_W 0x2    /* 0: read, 1: write. */
-#define PF_U 0x4    /* 0: kernel, 1: user process. */
-
 /* Page fault handler.  This is a skeleton that must be filled in
    to implement virtual memory.  Some solutions to project 2 may
    also require modifying this code.
