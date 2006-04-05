29c1ba099f587c7f6fb714c140ed47a69c59566f
Ben Pfaff <blp@cs.stanford.edu>
2006-04-05 20:59:27 +0000

    Comment. Thanks to "Vincenzo Di Nicola" <dinicola@stanford.edu> for the question.

--- src/filesys/inode.c
+++ src/filesys/inode.c
@@ -76,6 +76,9 @@ inode_create (disk_sector_t sector, off_t length)
   bool success = false;
 
   ASSERT (length >= 0);
+
+  /* If this assertion fails, the inode structure is not exactly
+     one sector in size, and you should fix that. */
   ASSERT (sizeof *disk_inode == DISK_SECTOR_SIZE);
 
   disk_inode = calloc (1, sizeof *disk_inode);
