5f8ae7add53756416ae4bb3240bb555e6064bd54
Ben Pfaff <blp@cs.stanford.edu>
2006-05-18 19:08:17 +0000

    Formatting.

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -380,8 +380,7 @@ validate_segment (const struct Elf32_Phdr *phdr, struct file *file)
    or disk read error occurs. */
 static bool
 load_segment (struct file *file, off_t ofs, uint8_t *upage,
-              uint32_t read_bytes, uint32_t zero_bytes,
-              bool writable) 
+              uint32_t read_bytes, uint32_t zero_bytes, bool writable) 
 {
   ASSERT ((read_bytes + zero_bytes) % PGSIZE == 0);
   ASSERT (pg_ofs (upage) == 0);
