31a083515ee53c90217a4242bf95285e3d36e010
Ben Pfaff <blp@cs.stanford.edu>
2006-05-19 20:39:54 +0000

    Comments.

--- src/filesys/directory.h
+++ src/filesys/directory.h
@@ -12,12 +12,16 @@
 #define NAME_MAX 14
 
 struct inode;
+
+/* Opening and closing directories. */
 bool dir_create (disk_sector_t sector, size_t entry_cnt);
 struct dir *dir_open (struct inode *);
 struct dir *dir_open_root (void);
 struct dir *dir_reopen (struct dir *);
 void dir_close (struct dir *);
 struct inode *dir_get_inode (struct dir *);
+
+/* Reading and writing. */
 bool dir_lookup (const struct dir *, const char *name, struct inode **);
 bool dir_add (struct dir *, const char *name, disk_sector_t);
 bool dir_remove (struct dir *, const char *name);
