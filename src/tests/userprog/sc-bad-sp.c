6ae5cde69b8ce2ec34771424c1d7ece7069ec976
Ben Pfaff <blp@cs.stanford.edu>
2006-11-03 03:07:56 +0000

    Clarify purpose and rationale. From Godmar Back.

--- src/tests/userprog/sc-bad-sp.c
+++ src/tests/userprog/sc-bad-sp.c
@@ -1,6 +1,13 @@
 /* Invokes a system call with the stack pointer (%esp) set to a
    bad address.  The process must be terminated with -1 exit
-   code. */
+   code. 
+
+   For Project 3: The bad address lies approximately 64MB below
+   the code segment, so there is no ambiguity that this attempt
+   must be rejected even after stack growth is implemented.
+   Moreover, a good stack growth heuristics should probably not
+   grow the stack for the purpose of reading the system call
+   number and arguments. */
 
 #include "tests/lib.h"
 #include "tests/main.h"
@@ -8,6 +15,6 @@
 void
 test_main (void) 
 {
-  asm volatile ("movl $0x20101234, %esp; int $0x30");
+  asm volatile ("movl . - (64*1024*1024), %esp; int $0x30");
   fail ("should have called exit(-1)");
 }
