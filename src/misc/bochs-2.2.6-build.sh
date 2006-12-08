0951a7af1bd8e78b5991edd7de9d0370b2d2d72b
Ben Pfaff <blp@cs.stanford.edu>
2006-12-08 18:47:37 +0000

    Make it possible to ignore page faults in GDB. From su.class.cs140 and Godmar Back.

--- src/misc/bochs-2.2.6-build.sh
+++ src/misc/bochs-2.2.6-build.sh
@@ -19,6 +19,8 @@ cat $PINTOSDIR/src/misc/bochs-2.2.6-big-endian.patch | patch -p1
 cat $PINTOSDIR/src/misc/bochs-2.2.6-jitter.patch | patch -p1
 cat $PINTOSDIR/src/misc/bochs-2.2.6-triple-fault.patch | patch -p1
 cat $PINTOSDIR/src/misc/bochs-2.2.6-solaris-tty.patch | patch -p1
+cat $PINTOSDIR/src/misc/bochs-2.2.6-page-fault-segv.patch | patch -p1
+cat $PINTOSDIR/src/misc/bochs-2.2.6-paranoia.patch | patch -p1
 if test "`uname -s`" = "SunOS"; then
     cat $PINTOSDIR/src/misc/bochs-2.2.6-solaris-link.patch | patch -p1
 fi
