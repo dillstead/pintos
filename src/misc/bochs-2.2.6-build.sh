a094a81a89ccd919a338aa0acfa6cdae282bdf50
Russ Ross <russ@dixie.edu>
2011-08-24 20:41:30 -0700

    Fix boot under Bochs when Pintos is compiled with recent GCC.
    When compiled according to the instructions, bochs fails to load
    the project 1 kernel.  This seems to be due to the latest gcc using an
    instruction that bochs is not compiled to support.  Adding the
    '--enable-cpu-level=6' configure option to Bochs, as this commit does,
    seems to work.

--- src/misc/bochs-2.2.6-build.sh
+++ src/misc/bochs-2.2.6-build.sh
@@ -26,7 +26,7 @@ cat $PINTOSDIR/src/misc/bochs-2.2.6-namespace.patch | patch -p1
 if test "`uname -s`" = "SunOS"; then
     cat $PINTOSDIR/src/misc/bochs-2.2.6-solaris-link.patch | patch -p1
 fi
-CFGOPTS="--with-x --with-x11 --with-term --with-nogui --prefix=$DSTDIR"
+CFGOPTS="--with-x --with-x11 --with-term --with-nogui --prefix=$DSTDIR --enable-cpu-level=6"
 mkdir plain &&
         cd plain && 
         ../configure $CFGOPTS --enable-gdb-stub && 
