47747baf50a7db012348a4954b97fc161bcacd19
John Ousterhout <ouster@cs.stanford.edu>
2015-12-17 11:38:47 -0800

    Updates for new version of qemu
    Use new isa-debug-exit device for shutdown, switch name to
    qemu-system-i386.

--- src/devices/shutdown.c
+++ src/devices/shutdown.c
@@ -107,6 +107,13 @@ shutdown_power_off (void)
   for (p = s; *p != '\0'; p++)
     outb (0x8900, *p);
 
+  /* For newer versions of qemu, you must run with -device
+   * isa-debug-exit, which exits on any write to an IO port (by
+   * default 0x501).  Qemu's exit code is double the value plus one,
+   * so there is no way to exit cleanly.  We use 0x31 which should
+   * result in a qemu exit code of 0x63.  */
+  outb (0x501, 0x31);
+
   /* This will power off a VMware VM if "gui.exitOnCLIHLT = TRUE"
      is set in its configuration file.  (The "pintos" script does
      that automatically.)  */
